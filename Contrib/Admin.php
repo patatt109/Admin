e4eeebfb58a15c79d3aabda3e67ce4b9f4225546
Anton Okulov <agency@ordertarget.ru>
2018-10-24 15:25:55 +0300

    Fixes related admin

--- Contrib/Admin.php
+++ Contrib/Admin.php
@@ -1118,7 +1118,7 @@ abstract class Admin
         $admins = [];
         foreach ($config as $relation => $class) {
             /** @var Admin $admin */
-            $admin = new $class;
+            $admin = new $class($class::classNameShort(), $this->_moduleName, $this->_request, $this->_router, $this->_auth, $this->_flash, $this->_translate);
             if ($instance && $instance->pk) {
                 $admin->ownerPk = $instance->pk;
             }
